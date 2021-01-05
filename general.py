abb6abadf87d0af8235feee3bac746c675817a3c
d33pblu-g1 <michael.phitides@gmail.com>
2021-01-05 12:06:08 +0200

    initializing repository

---
+++ general.py
@@ -0,0 +1,14 @@
+#def FunctionName (Input):
+#    action
+#    return Output
+
+def addOne(number):
+    out1 = number + 1
+    return out1
+
+var = 0
+var = addOne(var)
+print(var)
+print(addOne(var))
+print(addOne(var))
+print(addOne(var))
---
+++ main2.py
@@ -0,0 +1,10 @@
+Band = "Linkin Park"
+Genre = "Rock"
+Formed = "1996"
+Vocalist = "Mike Shinoda"
+Guitarist = "Brad Delson"
+Bassist = "Dave Farrell"
+DJ = "Joe Hahn"
+Drummer = "Rob Bourdon"
+
+print(Band)
